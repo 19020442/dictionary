3e88a0cd749c2ec6013714159b34317d9eee1c5c
19020442 <71977176+19020442@users.noreply.github.com>
2020-10-08 13:50:15 +0700

    Update DictionaryCommandline.java

--- DICTIONARY/src/DictionaryCommandline.java
+++ DICTIONARY/src/DictionaryCommandline.java
@@ -1,19 +1,56 @@
-public class DictionaryCommandline extends  DictionaryManagement {
 
+import java.io.IOException;
+import java.util.Scanner;
+
+public class DictionaryCommandline extends DictionaryManagement {
     public void showAllWords() {
-        System.out.printf("No\t\t|English                    |VietNamese            \n");
-        for (int x = 0 ; x< arr_of_words.size(); x++) {
-            System.out.printf("%d\t\t| %-20s\t\t| %-20s\n",(x+1),arr_of_words.get(x).word_target,arr_of_words.get(x).word_explain);
+        System.out.print("No\t\t|English                    |VietNamese            \n");
+        for (int x = 0; x < Dictionary.arr_of_words.size(); x++) {
+            System.out.printf("%d\t\t| %-20s\t\t| %-20s\n", (x + 1), Dictionary.arr_of_words.get(x).word_target, Dictionary.arr_of_words.get(x).word_explain);
         }
 
     }
+
     public void dictionaryBasic() {
         insertCommandLine();
         showAllWords();
     }
-    public static void main(String[] args) {
-        DictionaryCommandline center = new DictionaryCommandline();
-        center.dictionaryBasic();
+
+    public void dictionaryAdvanced() throws IOException {
+        insertFromFile();
+        showAllWords();
+        dictionaryLookup();
+        delete();
     }
+
+    public void dictionarySearcher() {
+        Scanner sc = new Scanner(System.in);
+        String sug = sc.next();
+        for (int x = 0; x < Dictionary.arr_of_words.size(); x++) {
+            int count = 0;
+            int len_of_sug = sug.length();
+            int i = 0;
+            while (len_of_sug > 0) {
+                if (Dictionary.arr_of_words.get(x).word_target.charAt(i) == sug.charAt(i)) {
+                    i++;
+
+                } else {
+                    i++;
+                    count++;
+                }
+                len_of_sug--;
+            }
+            if (count == 0) System.out.println(Dictionary.arr_of_words.get(x).word_target);
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        DictionaryCommandline center = new DictionaryCommandline();
+        center.dictionaryAdvanced();
+        center.showAllWords();
+        center.dictionarySearcher();
+
+    }
+
 
 }
